8f9f1414f9afb29fcb077be2c452d19d4c18fce6
erobic <robikshrestha@gmail.com>
2019-08-27 22:27:19 -0400

    Preprocess

--- scripts/preprocess/preprocess_questions.sh
+++ scripts/preprocess/preprocess_questions.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 source scripts/common.sh
 
+DATA_ROOT=${ROOT}/VQACP
 python preprocess/create_dictionary.py --data_root ${DATA_ROOT}
 python preprocess/compute_softscore.py --data_root ${DATA_ROOT}
